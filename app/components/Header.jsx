b4817adbc6990c398d1e299b0585d55ca1cfacc2
RishabhDhawad <rishabhdhawad29@gmail.com>
2025-12-23 12:39:00 +0530

    portfolio

---
+++ app/components/Header.jsx
@@ -0,0 +1,96 @@
+import { assets } from "@/assets/assets";
+import Image from "next/image";
+import React from "react";
+import { motion } from "motion/react";
+
+const Header = () => {
+  return (
+    <div
+      className="w-11/12 max-w-3xl text-center mx-auto h-screen flex flex-col items-center justify-center gap-4"
+      id="top"
+    >
+      <motion.div
+        initial={{ scale: 0 }}
+        whileInView={{ scale: 1 }}
+        transition={{ duration: 0.5, type: "spring", stiffness: 100 }}
+      >
+        <Image
+          src={assets.profile_img}
+          alt="profile imgage"
+          className="rounded-full w-32"
+        ></Image>
+      </motion.div>
+      <motion.h3
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className="flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo"
+      >
+        Hi! I'm Rishabh Dhawad
+        <Image src={assets.hand_icon} alt="hand icon" className="w-6"></Image>
+      </motion.h3>
+      <motion.h1
+        initial={{ opacity: 0, y: -30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8, delay: 0.3 }}
+        className="text-3xl sm:text-6xl lg:text-[66px] font-Ovo "
+      >
+        Fullstack Developer based in India.
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0, y: -20 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, delay: 0.3 }}
+        className="max-w-2xl mx-auto font-Ovo"
+      >
+        I’m a software developer passionate about building efficient, scalable,
+        and user-friendly web applications. I enjoy turning complex problems
+        into clean, functional solutions.
+      </motion.p>
+
+      <div className="flex flex-col sm:flex-row items-center gap-4 mt-4">
+        <motion.a
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 1 }}
+          href="#contact"
+          className="px-10 py-3 border
+                  border-white 
+                    rounded-full
+                  bg-black
+                   text-white 
+                    flex 
+                    items-center 
+                     gap-2 
+                     dark:bg-transparent
+                    "
+        >
+          Contact me
+          <Image
+            src={assets.right_arrow_white}
+            alt="right arrow"
+            className="w-4"
+          ></Image>
+        </motion.a>
+
+        <motion.a
+          initial={{ opacity: 0, y: 30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, delay: 1.2 }}
+          href="https://drive.google.com/file/d/1EaXhrVHOeACsOYYF6bLbJJE2wGwAqEyn/view?usp=sharing"
+          download
+          className="px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black"
+        >
+          My Resume
+          <Image
+            src={assets.download_icon}
+            alt="download icon"
+            className="w-4"
+          ></Image>
+        </motion.a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
